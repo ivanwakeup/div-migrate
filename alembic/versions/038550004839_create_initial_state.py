e183c38fb3d32fe3131d0b86a516bb61e15b23eb
ivanwakeup <ivan9miller@gmail.com>
2018-11-27 16:59:30 -0700

    add initial migration

---
+++ alembic/versions/038550004839_create_initial_state.py
@@ -0,0 +1,33 @@
+"""create initial state
+
+Revision ID: 038550004839
+Revises: 
+Create Date: 2018-11-27 15:40:32.924236
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '038550004839'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('companies',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('token', sa.String(), nullable=False)
+        )
+    op.create_table('daily_balances',
+        sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('company_id', sa.Integer, ForeignKey('companies.id'), nullable=False),
+        sa.Column('date', sa.Date),
+        sa.Column('balance', sa.Float)
+    )
+
+
+def downgrade():
+    pass
